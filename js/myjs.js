861f277cc6e8635c35fff4929557c5cea21b6630
simaoqingnian <simaoqingnian@163.com>
2015-07-23 00:34:55 +0800

    page3 timeline draft

--- js/myjs.js
+++ js/myjs.js
@@ -9,12 +9,62 @@ var mySwiper = new Swiper ('.swiper-container', {
     onInit: function(swiper){ //Swiper2.x的初始化是onFirstInit
         swiperAnimateCache(swiper);
         swiperAnimate(swiper);
-        building_animate(swiper.activeIndex);
+        //time_line_vertical_animate(swiper.activeIndex);
     },
     onTransitionEnd: function(swiper){
-        //     alert("df");
         swiperAnimate(swiper);
-        building_animate(swiper.activeIndex);
-        page2_white_bk_animation(swiper.activeIndex);
+        time_line_vertical_animate(swiper.activeIndex);
     }
 })
+
+function time_line_vertical_animate(index)
+{
+    if(index == 2)
+    {
+        setTimeout(function(){
+            $("#page3_time_line_vertical").css({
+                "transition-property":"height 2s linear 0s",
+                "-webkit-transition":"height 2s linear 0s",
+                "-moz-transition":"height 2s linear 0s",
+                "-o-transition":"height 2s linear 0s"});
+            document.getElementById("page3_time_line_vertical").style.height = "80%";
+        }, 500);
+        setTimeout(function(){
+            $("#page3_time_line_horizon_1").css({
+                "transition-property":"width 1s linear, left 1s linear",
+                "-webkit-transition":"width 1s linear, left 1s linear",
+                "-moz-transition":"width 1s linear, left 1s linear",
+                "-o-transition":"width 1s linear, left 1s linear"});
+            document.getElementById("page3_time_line_horizon_1").style.width = "40%";
+            document.getElementById("page3_time_line_horizon_1").style.left = "10%";
+        }, 1000);
+
+        setTimeout(function(){
+            $("#page3_time_line_horizon_2").css({
+                "transition-property":"width 1s linear, left 1s linear",
+                "-webkit-transition":"width 1s linear, left 1s linear",
+                "-moz-transition":"width 1s linear, left 1s linear",
+                "-o-transition":"width 1s linear, left 1s linear"});
+            document.getElementById("page3_time_line_horizon_2").style.width = "40%";
+        }, 1700);
+
+        setTimeout(function(){
+            $("#page3_time_line_horizon_3").css({
+                "transition-property":"width 1s linear, left 1s linear",
+                "-webkit-transition":"width 1s linear, left 1s linear",
+                "-moz-transition":"width 1s linear, left 1s linear",
+                "-o-transition":"width 1s linear, left 1s linear"});
+            document.getElementById("page3_time_line_horizon_3").style.width = "40%";
+            document.getElementById("page3_time_line_horizon_3").style.left = "10%";
+        }, 2300);
+    }
+    else
+    {
+        $("#page_1_blue").css({
+            "transition-property":"height 0s linear 0s",
+            "-webkit-transition":"height 0s linear 0s",
+            "-moz-transition":"height 0s linear 0s",
+            "-o-transition":"height 0s linear 0s"});
+        document.getElementById("page3_time_line_vertical").style.height = "0";
+    }
+};
